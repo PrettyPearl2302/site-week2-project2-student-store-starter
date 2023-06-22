6865a5176cfa04034ad01ea1233b418108447516
Pearl Wogu <pearlw@meta.com>
2023-06-21 21:06:24 -0700

    Called the Logo component in the Nav component

--- student-store-ui/src/components/Navbar/Navbar.jsx
+++ student-store-ui/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,13 @@
 import * as React from "react"
 import "./Navbar.css"
+// import { Link } from "react-router-dom"
+import Logo from "../Logo/logo"
+
 
 export default function Navbar() {
   return (
     <nav className="navbar">
+      <Logo />
       <p>Navbar</p>
     </nav>
   )
