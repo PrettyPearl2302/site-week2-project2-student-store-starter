973ab689faf51a8a731e92d130bf498fbe0bfb4e
Pearl Wogu <pearlw@meta.com>
2023-06-22 11:08:25 -0700

    Create arrow toggle for side bar.

--- student-store-ui/src/components/Sidebar/Sidebar.jsx
+++ student-store-ui/src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,28 @@
-import * as React from "react"
+import React, { useState } from "react"
 import "./Sidebar.css"
+import {FaArrowLeft , FaArrowRight} from "react-icons/fa"
+// import CheckoutForm from "../CheckoutForm/CheckoutForm"
+// import ShoppingCart from "../ShoppingCart/ShoppingCart"
 
 export default function Sidebar() {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  }
+
+       let arrowIcon;
+        if (isOpen) {
+      arrowIcon = <FaArrowLeft className="arrow" />;
+      } else {
+      arrowIcon = <FaArrowRight className="arrow" />;
+      } 
+
   return (
-    <section className="sidebar">
-      <p>Sidebar</p>
-    </section>
+    <aside className={`sidebar ${isOpen ? "open" : ""}`}>
+      <div className="toggle-btn" onClick={handleToggle}>
+          {arrowIcon}
+      </div>
+    </aside>
   )
 }
