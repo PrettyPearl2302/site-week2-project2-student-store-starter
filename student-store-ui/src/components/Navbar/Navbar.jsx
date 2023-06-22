51524f1b344a15bd5fc6fe531ccca7a6953dc228
Pearl Wogu <pearlw@meta.com>
2023-06-22 06:30:09 -0700

    Use react icons in Nav component and link them to social media.

--- student-store-ui/src/components/Navbar/Navbar.jsx
+++ student-store-ui/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,38 @@
-import * as React from "react"
+import React from "react"
+import { Link } from "react-router-dom"
+import { FaTwitter, FaInstagram, FaFacebook } from "react-icons/fa"
+import Logo from "../Logo/Logo"
 import "./Navbar.css"
-// import { Link } from "react-router-dom"
-import Logo from "../Logo/logo"
-
 
 export default function Navbar() {
-  return (
-    <nav className="navbar">
-      <Logo />
-      <p>Navbar</p>
-    </nav>
-  )
+	return (
+		<nav className="nav-container">
+			<Logo />
+			<div className="nav-socials">
+				<a href="https://twitter.com" className="nav-social-link">
+					<FaTwitter />
+				</a>
+				<a href="https://instagram.com" className="nav-social-link">
+					<FaInstagram />
+				</a>
+				<a href="https://facebook.com" className="nav-social-link">
+					<FaFacebook />
+				</a>
+			</div>
+			{/* <div className="nav-links">
+				<Link to="/" className="nav-link">
+					Home
+				</Link>
+				<Link to="/about" className="nav-link">
+					About
+				</Link>
+				<Link to="/contact" className="nav-link">
+					Contact
+				</Link>
+				<Link to="/contact" className="nav-link">
+					Buy now
+				</Link>
+			</div> */}
+		</nav>
+	)
 }
