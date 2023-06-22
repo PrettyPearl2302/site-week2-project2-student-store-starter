6156dc6158b9573a185e061bc6cd465a27f45fe5
Pearl Wogu <pearlw@meta.com>
2023-06-22 10:25:18 -0700

    Modify logo component.

--- student-store-ui/src/components/Logo/logo.jsx
+++ student-store-ui/src/components/Logo/logo.jsx
@@ -4,7 +4,7 @@ import "./Logo.css"
 export default function Logo() {
     return (
         <Link to="/">
-            <div className="code-path-ogo">
+            <div className="code-path-logo">
 				<img src="/codepath-logo.png" alt="main-page-logo" />
             </div>
         </Link>
