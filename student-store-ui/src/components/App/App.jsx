92a57313088498d4a82499f70a3bc352edbab764
Pearl Wogu <pearlw@meta.com>
2023-06-22 18:09:48 -0700

    Sucessfully fetch data from API

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom'
 import Navbar from "../Navbar/Navbar"
 import Sidebar from "../Sidebar/Sidebar"
@@ -6,6 +6,28 @@ import Home from "../Home/Home"
 import "./App.css"
 
 export default function App() {
+
+  const [isFetching, setIsFetching] = useState(true)
+  const [fetchedProducts, setFetchedProducts] = useState([])
+  
+  useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await fetch("https://codepath-store-api.herokuapp.com/store");
+        console.log('Fetched!')
+        const data = await response.json()
+        const products = data.products
+        setFetchedProducts(products)
+				setIsFetching(false)
+			} catch (error) {
+				console.error('Error fetching products')
+				setIsFetching(false)
+			}
+		}
+
+		fetchData()
+	}, [])
+  console.log(fetchedProducts)
   return (
     <div className="app">
       <BrowserRouter>
