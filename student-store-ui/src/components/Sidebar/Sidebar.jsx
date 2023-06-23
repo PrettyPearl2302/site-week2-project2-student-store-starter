f0120d4be1fa95da26c2fa515be3b63909eafef5
Pearl Wogu <pearlw@meta.com>
2023-06-22 19:47:21 -0700

    Update sidebar to work flexibly.

--- student-store-ui/src/components/Sidebar/Sidebar.jsx
+++ student-store-ui/src/components/Sidebar/Sidebar.jsx
@@ -1,28 +1,32 @@
 import React, { useState } from "react"
+import { MdAddShoppingCart } from "react-icons/md"
+import { AiOutlineArrowRight, AiOutlineArrowLeft } from "react-icons/ai"
+import { BsCashCoin, BsCreditCard } from "react-icons/bs"
 import "./Sidebar.css"
-import {FaArrowLeft , FaArrowRight} from "react-icons/fa"
-// import CheckoutForm from "../CheckoutForm/CheckoutForm"
-// import ShoppingCart from "../ShoppingCart/ShoppingCart"
+import ShoppingCart from "../ShoppingCart/ShoppingCart"
+import CheckoutForm from "../CheckoutForm/CheckoutForm"
 
 export default function Sidebar() {
-  const [isOpen, setIsOpen] = useState(false)
+	const [isOpen, setIsOpen] = useState(false)
 
-  const handleToggle = () => {
-    setIsOpen(!isOpen);
-  }
+	const handleToggle = () => {
+		setIsOpen(!isOpen)
+	}
 
-       let arrowIcon;
-        if (isOpen) {
-      arrowIcon = <FaArrowLeft className="arrow" />;
-      } else {
-      arrowIcon = <FaArrowRight className="arrow" />;
-      } 
-
-  return (
-    <aside className={`sidebar ${isOpen ? "open" : ""}`}>
-      <div className="toggle-btn" onClick={handleToggle}>
-          {arrowIcon}
-      </div>
-    </aside>
-  )
+	return (
+		<aside className={`sidebar ${isOpen ? "open" : ""}`}>
+			<div className="toggle-btn" onClick={handleToggle}>
+				{isOpen ? <AiOutlineArrowLeft className="arrow" /> : <AiOutlineArrowRight className="arrow" />}
+			</div>
+			<div className={`sidebar-icons ${isOpen ? "open" : ""}`}>
+				<MdAddShoppingCart className="sidebar-icon" onClick={handleToggle}/>
+				<BsCashCoin className="sidebar-icon" onClick={handleToggle}/>
+				<BsCreditCard className="sidebar-icon" onClick={handleToggle}/>
+			</div>
+			<div className="sidebar-content">
+				<ShoppingCart />
+				<CheckoutForm />
+			</div>
+		</aside>
+	)
 }
