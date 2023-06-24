dd998df862cc0de4d5e7e01a4f83642a830d50e5
Pearl Wogu <pearlw@meta.com>
2023-06-23 18:04:57 -0700

    Create Categories component

---
+++ student-store-ui/src/components/Categories/Categories.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Categories.css";
+
+export default function Categories({ selectCategory }) {
+  function handleClick(category) {
+    console.log(Clicked on: ${category});
+    selectCategory(category);
+  }
+
+  return (
+    <nav className="categories">
+      <nav>
+        <ul>
+          <li>
+            <button onClick={() => handleClick("Clothing")}>Clothing</button>
+          </li>
+          <li>
+            <button onClick={() => handleClick("Food")}>Food</button>
+          </li>
+          <li>
+            <button onClick={() => handleClick("Accessories")}>Accessories</button>
+          </li>
+          <li>
+            <button onClick={() => handleClick("Tech")}>Tech</button>
+          </li>
+          <li>
+            <button onClick={() => handleClick("All categories")}>All categories</button>
+          </li>
+        </ul>
+      </nav>
+    </nav>
+  );
+}
