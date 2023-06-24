c3b5c0411f7f46f151d7a92475d7bc197011e0cc
Pearl Wogu <pearlw@meta.com>
2023-06-23 18:12:42 -0700

    Display categories in home component.

--- student-store-ui/src/components/Categories/Categories.jsx
+++ student-store-ui/src/components/Categories/Categories.jsx
@@ -3,7 +3,6 @@ import "./Categories.css";
 
 export default function Categories({ selectCategory }) {
   function handleClick(category) {
-    console.log(Clicked on: ${category});
     selectCategory(category);
   }
 
