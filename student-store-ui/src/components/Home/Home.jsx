e70972446600b38438ba3614997c6ab849361c1b
Pearl Wogu <pearlw@meta.com>
2023-06-23 20:48:37 -0700

    Home component with all imported components from other files

--- student-store-ui/src/components/Home/Home.jsx
+++ student-store-ui/src/components/Home/Home.jsx
@@ -1,10 +1,22 @@
-import * as React from "react"
-import "./Home.css"
+import React, { useState } from "react";
+import Hero from "../Hero/Hero";
+import "./Home.css";
+import About from "../About/About";
+import Contact from "../Contact/Contact";
+import Footer from "../Footer/Footer";
+import ProductGrid from "../ProductGrid/ProductGrid";
+import Search from "../Search/Search";
 
-export default function Home() {
+export default function Home({ products, isFetching }) {
+
   return (
     <div className="home">
-      <p>Home</p>
+      <Hero />
+      <Search />
+      <ProductGrid products={products} isFetching={isFetching} />
+      <About />
+      <Contact />
+      <Footer />
     </div>
-  )
+  );
 }
