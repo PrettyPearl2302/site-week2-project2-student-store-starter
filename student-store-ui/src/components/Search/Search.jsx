b38d942093bb072d0b5f8b296c419006b01adfae
Pearl Wogu <pearlw@meta.com>
2023-06-23 20:28:54 -0700

    Delete categories component and merge search and categories components.

--- student-store-ui/src/components/Search/Search.jsx
+++ student-store-ui/src/components/Search/Search.jsx
@@ -1,40 +1,61 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from "react";
+import { ProductContext } from "../../state/ProductContext";
 import "./Search.css";
 
 const Search = () => {
-  const [searchQuery, setSearchQuery] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [categoriesVisible, setCategoriesVisible] = useState(false);
 
-  const handleSearch = (event) => {
-    const { value } = event.target;
-    setSearchQuery(value);
+  const { products, setFilteredProducts } = useContext(ProductContext);
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value)
+    const filteredProducts = products.products.filter((product) =>
+      product.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  
+    setFilteredProducts({products: filteredProducts});
+  };
+  
+  const handleFilter = (category) => {
+    const filteredProducts = products.products.filter((product) =>
+      category === "all" ? true : product.category === category
+    );
+
+    console.log(filteredProducts);
+  
+    setFilteredProducts({products: filteredProducts});
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Perform search functionality here
-    const filteredProducts = products.filter((product) => {
-      return product.name.toLowerCase().includes(searchQuery.toLowerCase());
-    });
-    setSearchResults(filteredProducts);
+  const toggleCategoriesVisibility = () => {
+    setCategoriesVisible(!categoriesVisible);
   };
 
   return (
     <div className="search-bar">
-      <form onSubmit={handleSubmit}>
-        <div className="search-container">
-          <input
-            type="text"
-            className="search-input"
-            value={searchQuery}
-            onChange={handleSearch}
-            placeholder="Search products here"
-          />
-          <button className="searchBtn" type="submit">
-            Search
-          </button>
+      <div className="search-container">
+        <input
+        className="search-input"
+          type="text"
+          placeholder="Search for a product..."
+          value={searchTerm}
+          onChange={(e) => handleChange(e)}
+        />
+      </div>
+      <div className={`filter-categories ${categoriesVisible ? "visible" : "hidden"}`}>
+        <div onClick={toggleCategoriesVisibility} className="toggle-button">
+            menu
         </div>
-      </form>
+        {categoriesVisible && (
+          <div className="search-categories">
+            <p onClick={() => handleFilter("all")}>All Categories</p>
+            <p onClick={() => handleFilter("clothing")}>Clothing</p>
+            <p onClick={() => handleFilter("food")}>Food</p>
+            <p onClick={() => handleFilter("accessories")}>Accessories</p>
+            <p onClick={() => handleFilter("tech")}>Tech</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
