b6a344da5ec0d4c030c1bf23828bd0cccb8d4f0e
Pearl Wogu <pearlw@meta.com>
2023-06-23 20:49:20 -0700

    ProductCard with css | Link from react-dom for dynamic routing

--- student-store-ui/src/components/ProductCard/ProductCard.jsx
+++ student-store-ui/src/components/ProductCard/ProductCard.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import { HiOutlineMinus, HiOutlinePlus } from "react-icons/hi"
 import "./ProductCard.css"
+import { Link } from 'react-router-dom';
 
-function Product({ id, name, price }) {
+const ProductCard = ({ product }) => {
   return (
-    <div>
-      <h3>{name}</h3>
-      <p>Price: {price}</p>
+    <div key={product.id} className="product-item">
+      <Link to={`/products/${product.id}`} style={{ textDecoration: "none", color: "inherit" }}>
+        <img src={product.image} alt={product.name} />
+        <div className="product-name-count">
+            <p>{product.name}</p>
+            <div>
+                <HiOutlinePlus className="sign plus" onClick={(event) => { event.preventDefault(); addToCart(product); }}/>
+              <div>
+                <HiOutlineMinus className="sign minus" onClick={(event) => { event.preventDefault(); removeFromCart(product); }} />
+              </div>
+            </div>
+        </div>
+            <p className="product-price">{formatPrice(product.price)}</p>
+            <div className="stars">
+                <img src="/stars.png" alt="stars" />
+            </div>
+        </Link>
     </div>
-  );
+  )
 }
 
-export default Product;
+export function formatPrice(price) {
+	const formattedPrice = price % 1 === 0 ? price.toFixed(0) : price.toFixed(2)
+	return `$${formattedPrice}`;
+}
+
+export default ProductCard
