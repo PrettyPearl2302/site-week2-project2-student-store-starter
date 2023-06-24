b487761b169df1655d17e4409015621e80673e8f
Pearl Wogu <pearlw@meta.com>
2023-06-23 20:52:21 -0700

    implement Provider useContext and css

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -1,24 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter } from 'react-router-dom'
-import Navbar from "../Navbar/Navbar"
+import React, { useState, useEffect, useContext } from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import axios from "axios"
+import Navbar from "../Navbar/Navbar"
 import Sidebar from "../Sidebar/Sidebar"
 import Home from "../Home/Home"
+import ProductDetail from "../ProductDetail/ProductDetail";
+import { ProductContext } from "../../state/ProductContext"
 import "./App.css"
 
 export default function App() {
+	const { setFetchedProducts, filteredProducts, setFilteredProducts } = useContext(ProductContext)
+	const [isFetching, setIsFetching] = useState(true)
 
-  const [isFetching, setIsFetching] = useState(true)
-  const [fetchedProducts, setFetchedProducts] = useState([])
-  
-  useEffect(() => {
+	useEffect(() => {
 		const fetchData = async () => {
 			try {
 				const response = await axios.get(
 					"https://codepath-store-api.herokuapp.com/store"
 				)
-
 				setFetchedProducts(response.data)
+				setFilteredProducts(response.data)
 				setIsFetching(false)
 			} catch (error) {
 				console.error('Error fetching products')
@@ -29,13 +30,28 @@ export default function App() {
 		fetchData()
 	}, [])
 
-  return (
-    <div className="app">
-      <BrowserRouter>
+	return (
+		<div className="app">
+			<Router>
 				<Navbar />
 				<Sidebar />
-				<Home products={fetchedProducts} isFetching={isFetching}/>
-      </BrowserRouter>
-    </div>
-  );
+				<div className="content">
+					<Routes>
+						<Route
+							path="/"
+							element={<Home products={filteredProducts} isFetching={isFetching} />}
+						/>
+						<Route
+							path="/products/:productId"
+							element={
+								<ProductDetail
+									products={filteredProducts}
+								/>
+							}
+						/>
+					</Routes>
+				</div>
+			</Router>
+		</div>
+	)
 }
